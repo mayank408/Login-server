f5c67db7aa542639ff753caf5723144312d447bd
Mayank <mayank.trp48@gmail.com>
2017-06-02 13:32:15 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var connect = require('connect');
 var mongoose = require('mongoose');
+var crypto = require('crypto')
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/login');
@@ -19,14 +20,26 @@ var user = mongoose.model('user', schema);
 app.get('/register', function(req,res){
 var user1 = new user({username : req.query.login , password : req.query.pass})
 
-user1.save(function(err){
-  if(err)
-    console.log(err);
-  else
-    console.log(user1);
-});
+user.findOne({
+    username: req.query.login
+  }, function(err, user) {
+    if (err) throw err;
+ 
+    if (!user) {
+		user1.save(function(err){
+		  if(err) throw err
+		  else
+		    console.log(user1);
+		});
+		res.send({success: true, msg: 'Authentication successfully'});    
+}
+else{
+		res.send({success: false, msg: 'User Already registered. Please log in'});
+    }
+	
+})
 
-res.send({success: true, msg: 'Authentication successfully'});
+
 	
 });
 
@@ -41,13 +54,17 @@ user.findOne({
     if (err) throw err;
  
     if (!user) {
-      res.send({success: false, msg: 'Authentication failed. User not found.'});
+      res.send({success: false, msg: 'Login failed, User not found. Please try again.'});
     }else{
     	var password = user.password
-    	if(req.query.pass == password)
-    	      res.send({success: true, msg: 'Authentication successfully. User found.'});
+    	if(req.query.pass == password){
+    		var token = crypto.randomBytes(10).toString('hex');
+    	    res.send({success: true,
+    	    	token: token, 
+    	    	msg: 'Authentication successfully. User found.'});
+    	  }
     	  else
-    	  	      res.send({success: false, msg: 'Wrong password'});
+    	  	      res.send({success: false, msg: 'Wrong password , Please try again later'});
 
     }
 	
